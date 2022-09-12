5b16c5aead7b297dcc21a983fb338fb3f2aa731f
unknown <helamrani382@gmail.com>
2022-09-12 01:47:02 +0100

    some changes

--- lib/screens/Checkout.dart
+++ lib/screens/Checkout.dart
@@ -237,7 +237,7 @@ class _CheckoutState extends State<Checkout> {
                             hintStyle: TextStyle(color: Color(0xff73BFBD)),
                             enabledBorder: OutlineInputBorder(
                               borderRadius: BorderRadius.circular(10),
-                              borderSide: BorderSide(width: 0.001),
+                               borderSide: BorderSide(width: 0.001,color: Color.fromARGB(255, 249, 249, 249),),
                             ),
                             focusedBorder: OutlineInputBorder(
                               borderRadius: BorderRadius.circular(10),
@@ -337,11 +337,11 @@ class _CheckoutState extends State<Checkout> {
                             hintStyle: TextStyle(color: Color(0xff73BFBD)),
                             enabledBorder: OutlineInputBorder(
                               borderRadius: BorderRadius.circular(10),
-                              borderSide: BorderSide(width: 0.0001),
+                              borderSide: BorderSide(width: 0.001,color: Color.fromARGB(255, 249, 249, 249),),
                             ),
                             focusedBorder: OutlineInputBorder(
                               borderRadius: BorderRadius.circular(10),
-                              borderSide: BorderSide(width: 0.0001),
+                               borderSide: BorderSide(width: 0.001,color: Color.fromARGB(255, 249, 249, 249),),
                             )),
                       ),
                     ),
@@ -376,11 +376,11 @@ class _CheckoutState extends State<Checkout> {
                             hintStyle: TextStyle(color: Color(0xff73BFBD)),
                             enabledBorder: OutlineInputBorder(
                               borderRadius: BorderRadius.circular(10),
-                              borderSide: BorderSide(width: 0.0001),
+                              borderSide: BorderSide(width: 0.001,color: Color.fromARGB(255, 249, 249, 249),),
                             ),
                             focusedBorder: OutlineInputBorder(
                               borderRadius: BorderRadius.circular(10),
-                              borderSide: BorderSide(width: 0.0001),
+                               borderSide: BorderSide(width: 0.001,color: Color.fromARGB(255, 249, 249, 249),),
                             )),
                       ),
                     ),
@@ -416,11 +416,11 @@ class _CheckoutState extends State<Checkout> {
                             hintStyle: TextStyle(color: Color(0xff73BFBD)),
                             enabledBorder: OutlineInputBorder(
                               borderRadius: BorderRadius.circular(10),
-                              borderSide: BorderSide(width: 0.0001),
+                             borderSide: BorderSide(width: 0.001,color: Color.fromARGB(255, 249, 249, 249),),
                             ),
                             focusedBorder: OutlineInputBorder(
                               borderRadius: BorderRadius.circular(10),
-                              borderSide: BorderSide(width: 0.0001),
+                              borderSide: BorderSide(width: 0.001,color: Color.fromARGB(255, 249, 249, 249),),
                             )),
                       ),
                     ),
@@ -515,11 +515,11 @@ class _CheckoutState extends State<Checkout> {
                                 color: Color(0xffDCDCDC), fontSize: 10.sp),
                             enabledBorder: OutlineInputBorder(
                               borderRadius: BorderRadius.circular(10),
-                              borderSide: BorderSide(width: 0.0001),
+                               borderSide: BorderSide(width: 0.001,color: Color.fromARGB(255, 249, 249, 249),),
                             ),
                             focusedBorder: OutlineInputBorder(
                               borderRadius: BorderRadius.circular(10),
-                              borderSide: BorderSide(width: 0.0001),
+                             borderSide: BorderSide(width: 0.001,color: Color.fromARGB(255, 249, 249, 249),),
                             )),
                       ),
                     ),
@@ -647,223 +647,4 @@ class _CheckoutState extends State<Checkout> {
   }
 }
 
-//  Flexible(
-//             child: ListView(
 
-//                 // Remove padding
-//                 padding: EdgeInsets.zero,
-//                 children: [
-
-//                   SizedBox(
-//                     height: 1.h,
-//                   ),
-
-//                   Padding(
-//                     padding:  EdgeInsets.only(left:25.sp),
-//                     child: Text("Receiver Address",
-//                     style: TextStyle(
-//                       color: Color(0xffD8AA6B),
-//                       fontSize: 10.sp,
-//                     ),
-//                     ),
-//                   ),
-//                   Container(
-//                     margin: EdgeInsets.symmetric(horizontal: 25.sp, vertical:10.sp),
-//                     color:Color.fromARGB(255, 249, 249, 249),
-//                     height: 5.h,
-//                     child: TextFormField(
-//                       decoration:  InputDecoration(
-
-//                           enabledBorder: OutlineInputBorder(
-//                             borderRadius: BorderRadius.circular(10),
-//                             borderSide: BorderSide(width: 0.05),
-
-//                           ),
-//                           focusedBorder: OutlineInputBorder(
-//                             borderRadius: BorderRadius.circular(10),
-//                            borderSide: BorderSide(width: 0.05),
-
-//                           )),
-//                     ),
-//                   ),
-//                   Padding(
-//                     padding:  EdgeInsets.only(left:25.sp),
-//                     child: Text("Additional Address Info",
-//                     style: TextStyle(
-//                       color: Color(0xffD8AA6B),
-//                       fontSize: 10.sp,
-//                     ),),
-//                   ),
-//                   Container(
-//                     margin: EdgeInsets.symmetric(horizontal: 25.sp, vertical:10.sp),
-//                     color:Color.fromARGB(255, 249, 249, 249),
-//                      height: 5.h,
-//                     child: TextFormField(
-//                       decoration:  InputDecoration(
-
-//                           enabledBorder: OutlineInputBorder(
-//                             borderRadius: BorderRadius.circular(10),
-//                             borderSide: BorderSide(width: 0.05),
-
-//                           ),
-//                           focusedBorder: OutlineInputBorder(
-//                             borderRadius: BorderRadius.circular(10),
-//                            borderSide: BorderSide(width: 0.05),
-
-//                           )),
-//                     ),
-//                   ),
-//                  Padding(
-//                     padding:  EdgeInsets.only(left:25.sp),
-//                     child: Text("Receiver Phone Number", style: TextStyle(
-//                       color: Color(0xffD8AA6B),
-//                       fontSize: 10.sp,
-//                     ),),
-//                   ),
-//                   Container(
-//                     margin: EdgeInsets.symmetric(horizontal: 25.sp, vertical:10.sp),
-//                     color:Color.fromARGB(255, 249, 249, 249),
-//                     height: 5.h,
-//                     child: TextFormField(
-//                       decoration:  InputDecoration(
-
-//                           enabledBorder: OutlineInputBorder(
-//                             borderRadius: BorderRadius.circular(10),
-//                             borderSide: BorderSide(width: 0.05),
-
-//                           ),
-//                           focusedBorder: OutlineInputBorder(
-//                             borderRadius: BorderRadius.circular(10),
-//                            borderSide: BorderSide(width: 0.05),
-
-//                           )),
-//                     ),
-//                   ),
-//                   Padding(
-//                     padding:  EdgeInsets.only(left:25.sp),
-//                     child: Text("Your Phone Number*", style: TextStyle(
-//                       color: Color(0xffD8AA6B),
-//                       fontSize: 10.sp,
-//                     ),),
-//                   ),
-//                   Container(
-//                     margin: EdgeInsets.symmetric(horizontal: 25.sp, vertical:10.sp),
-//                     color:Color.fromARGB(255, 249, 249, 249),
-//                      height: 5.h,
-//                     child: TextFormField(
-//                       decoration:  InputDecoration(
-
-//                           enabledBorder: OutlineInputBorder(
-//                             borderRadius: BorderRadius.circular(10),
-//                             borderSide: BorderSide(width: 0.05),
-
-//                           ),
-//                           focusedBorder: OutlineInputBorder(
-//                             borderRadius: BorderRadius.circular(10),
-//                            borderSide: BorderSide(width: 0.05),
-
-//                           )),
-//                     ),
-//                   ),
-//                  Padding(
-//                     padding:  EdgeInsets.only(left:25.sp),
-//                     child: Text("Select Your Massage", style: TextStyle(
-//                       color: Color(0xffD8AA6B),
-//                       fontSize: 10.sp,
-//                     ),),
-//                   ),
-//                   Container(
-//                     margin: EdgeInsets.symmetric(horizontal: 25.sp, vertical:10.sp),
-//                     color:Color.fromARGB(255, 249, 249, 249),
-//                     height: 5.h,
-//                     child: DropdownButtonHideUnderline(
-//                         child: DropdownButton2(
-//                           // dropdownWidth:0.w-20.sp,
-//                           // barrierDismissible:false,
-//                           iconSize: 25.sp,
-//                           iconEnabledColor: Color(0xff73BFBD),
-
-//                           dropdownElevation: 0,
-
-//                           isExpanded: true,
-//                           hint: Text(
-//                             '  Select Your Massage',
-//                             style: TextStyle(
-//                               fontSize: 12,
-//                               color: Color(0xffDCDCDC),
-//                             ),
-//                           ),
-//                           items: _addDividersAfterItems(items),
-//                           customItemsIndexes: _getDividersIndexes(),
-//                           customItemsHeight: 4,
-//                           value: selectedValue,
-//                           onChanged: (value) {
-//                             setState(() {
-//                               selectedValue = value as String;
-//                             });
-//                           },
-//                           buttonHeight: 40,
-//                           buttonWidth: 140,
-//                           itemHeight: 40,
-//                           itemPadding: EdgeInsets.symmetric(horizontal: 8.sp),
-//                         ),
-//                       ),
-//                   ),
-//                  Padding(
-//                     padding:  EdgeInsets.only(left:25.sp),
-//                     child: Text("Put A Phrase On The Flower Bouquet", style: TextStyle(
-//                       color: Color(0xffD8AA6B),
-//                       fontSize: 10.sp,
-//                     ),),
-//                   ),
-//                   Container(
-//                     margin: EdgeInsets.symmetric(horizontal: 25.sp, vertical:10.sp),
-//                     color:Color.fromARGB(255, 249, 249, 249),
-//                     height: 8.h,
-//                     child: TextFormField(
-//                       maxLines: 5,
-//                       decoration:  InputDecoration(
-
-//                           enabledBorder: OutlineInputBorder(
-//                             borderRadius: BorderRadius.circular(10),
-//                             borderSide: BorderSide(width: 0.05),
-
-//                           ),
-//                           focusedBorder: OutlineInputBorder(
-//                             borderRadius: BorderRadius.circular(10),
-//                            borderSide: BorderSide(width: 0.05),
-
-//                           )),
-//                     ),
-//                   ),
-//                   Container(
-//                     margin: EdgeInsets.symmetric(
-//                         horizontal: 30.sp, vertical: 8.sp),
-//                     child: TextButton(
-//                       onPressed: () {
-//                         Navigator.push(
-//                               context,
-//                               MaterialPageRoute(
-//                                   builder: (context) =>  PaymentScreen()),
-//                             );
-//                       },
-//                       child: Padding(
-//                         padding: EdgeInsets.symmetric(vertical: 8.sp),
-//                         child: Text(
-//                           "Checkout",
-//                           style: TextStyle(color: Colors.white,fontSize: 12.sp),
-//                         ),
-//                       ),
-//                       style: ButtonStyle(
-//                         shape:
-//                             MaterialStateProperty.all<RoundedRectangleBorder>(
-//                                 RoundedRectangleBorder(
-//                           borderRadius: BorderRadius.circular(38.0),
-//                         )),
-//                         backgroundColor:
-//                             MaterialStateProperty.all<Color>(Color(0xff73BFBD)),
-//                       ),
-//                     ),
-//                   ),
-//                 ]),
-//           ),
